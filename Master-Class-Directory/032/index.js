9409d3369bcdc265db172025de054c392d8b504d
homdahal997 <Hom Dahal>
2024-01-21 08:11:04 -0500

    update

---
+++ Master-Class-Directory/032/index.js
@@ -0,0 +1,32 @@
+             //parameter
+            //    !
+// function Welcome(name) {
+//     console.log(`Welcome ${name}`); 
+// }
+// Welcome("Hom Dahal");
+
+// function addition(numOne, numTwo) {
+//     let result = numOne + numTwo;
+//     console.log(result);
+
+// }
+// addition(5, 10)
+
+// function fullName(firstName, lastName) {
+//     console.log(`WELCOME ${firstName} ${lastName}`);
+// }
+// fullName("Hom", "Dahal");
+
+// function addition(numOne = 0, numTwo = 0){
+//     let result = numOne + numTwo;
+//     return result;
+// }
+// const result = (addition(5, 10));
+// console.log(result);
+
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+    
+}
+let value = fullName("Hom", "Dahal");
+console.log(value);
---
+++ Master-Class-Directory/033/index.js
@@ -0,0 +1,15 @@
+// Arrow function
+let Welcome = function() {
+    console.log("Welcome");
+};  
+Welcome();
+
+let intro = () => {
+    console.log("WELCOME ARROW FUNCTION");
+};
+intro();
+
+let Welcome1 = () => {
+    console.log("Welcome Arrow Function 2");
+};
+Welcome1();
