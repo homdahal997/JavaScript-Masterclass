218928ae6a586d3927034a3d93639e0872ef582b
homdahal997 <Hom Dahal>
2024-01-28 17:38:44 -0500

    New learning materials

---
+++ Master-Class-Directory/044/index.js
@@ -0,0 +1,9 @@
+// Document Object model (DOM): An interface which help us
+// manipulate our website using JavaScript
+
+console.log(document);
+
+console.log(document.location);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
